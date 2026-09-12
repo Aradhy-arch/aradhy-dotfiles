67ca2c031e292fe43d431b47bf87a41e21d3487f
Aradhy-arch <zzgg.andrew.ggzz@gmail.com>
2026-09-12 15:59:42 +0000

    Update set_bg.sh

--- wallust/templates/set_bg.sh
+++ wallust/templates/set_bg.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 hyprctl hyprpaper preload "{{wallpaper}}"
-hyprctl hyprpaper wallpaper "LVDS-1,{{wallpaper}}"
+hyprctl hyprpaper wallpaper ",{{wallpaper}}"
 hyprctl hyprpaper unload all
 
 cat << EOF > "$HOME/.config/hypr/hyprpaper.conf"
 wallpaper {
-    monitor = LVDS-1
+    monitor =
     path = {{wallpaper}}
     fit_mode = cover
 }
