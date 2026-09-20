3a39c2eabbc619fc8efd95dbec0ca33c09794212
Aradhy-arch <zzgg.andrew.ggzz@gmail.com>
2026-09-20 12:28:10 +0530

    Add files via upload

---
+++ hypr/configs/wrules.lua
@@ -0,0 +1,52 @@
+--------------------------------
+---- WINDOWS AND WORKSPACES ----
+--------------------------------
+local suppressMaximizeRule = hl.window_rule({
+    -- Ignore maximize requests from all apps. You'll probably like this.
+    name  = "suppress-maximize-events",
+    match = { class = ".*" },
+
+    suppress_event = "maximize",
+})
+
+hl.window_rule({
+    name  = "fix-xwayland-drags",
+    match = {
+        class      = "^$",
+        title      = "^$",
+        xwayland   = true,
+        float      = true,
+        fullscreen = false,
+        pin        = false,
+    },
+
+    no_focus = true,
+})
+
+hl.window_rule({
+  match = { class = "^(rofi)$" },
+  stay_focused = true
+})
+
+hl.window_rule({
+    name  = "move-hyprland-run",
+    match = { class = "hyprland-run" },
+
+    move  = "20 monitor_h-120",
+    float = true,
+})
+
+-- Swaync Blur
+hl.layer_rule({
+    name = "blur-swaync-cc",
+    match = { namespace = "swaync-control-center" },
+    blur = true,
+    ignore_alpha = 0.5
+})
+
+hl.layer_rule({
+    name = "blur-swaync-notif",
+    match = { namespace = "swaync-notification-window" },
+    blur = true,
+    ignore_alpha = 0.5
+})
