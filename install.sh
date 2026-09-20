fb28638c1028f85c5def2b1b2fdf4a1bd1e60d89
Aradhy-arch <zzgg.andrew.ggzz@gmail.com>
2026-09-20 14:51:42 +0530

    Update package lists in install.sh
    Removed 'eza' from the list of PACMAN_PKGS and added 'xarchiver' and 'thunar-archive-plugin'.

--- install.sh
+++ install.sh
@@ -17,12 +17,12 @@ gum spin --spinner dot --title "Cloning Repo..." -- git clone --depth=1 https://
 cd aradhy-dotfiles
 
 PACMAN_PKGS=(
-    fish btop zoxide fzf fastfetch starship eza bat micro neovim ark qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg
+    fish btop zoxide fzf fastfetch starship eza bat micro neovim qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg
     hyprland hyprpaper hyprlock swaync cliphist grim slurp waybar rofi hyprpolkitagent hypridle hyprsunset
     xdg-desktop-portal-hyprland xdg-desktop-portal-gtk qt5-wayland qt6-wayland noto-fonts-cjk noto-fonts-emoji
     pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
     bluez bluez-utils blueman wf-recorder brightnessctl
-    alacritty discord spotify-launcher mousepad thunar sddm wl-clip-persist imv mpv cmatrix
+    alacritty discord spotify-launcher mousepad thunar xarchiver thunar-archive-plugin sddm wl-clip-persist imv mpv cmatrix
 )
 
 AUR_PKGS=(
