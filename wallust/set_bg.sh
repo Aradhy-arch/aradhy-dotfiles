5cde1c7dfa47e4cdb29767d7bb643c30b1c85c32
Aradhy-arch <zzgg.andrew.ggzz@gmail.com>
2026-09-25 11:02:59 +0530

    Add files via upload

--- wallust/set_bg.sh
+++ wallust/set_bg.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 hyprctl hyprpaper unload all > /dev/null 2>&1
-hyprctl hyprpaper preload "/home/aradhy/Pictures/Wallpapers/evening-sky.png" > /dev/null 2>&1
+hyprctl hyprpaper preload "/home/aradhy/Pictures/Wallpapers/gradient-synth-cat.png" > /dev/null 2>&1
 
 for MON in $(hyprctl monitors | awk '/Monitor/ {print $2}'); do
-    hyprctl hyprpaper wallpaper "$MON,/home/aradhy/Pictures/Wallpapers/evening-sky.png" >/dev/null 2>&1
+    hyprctl hyprpaper wallpaper "$MON,/home/aradhy/Pictures/Wallpapers/gradient-synth-cat.png" >/dev/null 2>&1
 done
 
 cat << EOF > "$HOME/.config/hypr/hyprpaper.conf"
 wallpaper {
     monitor =
-    path = /home/aradhy/Pictures/Wallpapers/evening-sky.png
+    path = /home/aradhy/Pictures/Wallpapers/gradient-synth-cat.png
     fit_mode = cover
 }
 
